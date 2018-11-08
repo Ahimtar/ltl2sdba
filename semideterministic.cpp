786dba8be8931504d53d31c9ff7c5126a4948766
Michal Romanek <misoromanek@gmail.com>
2018-11-08 23:09:28 +0100

    Overall fixes, refactoring and debug logs added

--- semideterministic.cpp
+++ semideterministic.cpp
@@ -62,6 +62,7 @@ spot::twa_graph_ptr make_semideterministic(VWAA *vwaa, std::string debug) {
     // We iterate over all states of the VWAA
     for (unsigned q = 0; q < nq; ++q)
     {
+        if (debug == "1"){std::cout << "State: " << (*snvwaa)[q] << " (" << q << ").\n";}
         // Renaming state to its number instead of the LTL formula for later use
         (*snvwaa)[q] = std::to_string(q);
 
@@ -72,8 +73,8 @@ spot::twa_graph_ptr make_semideterministic(VWAA *vwaa, std::string debug) {
             for (unsigned d: pvwaa->univ_dests(t.dst))
             {
                 if (t.src == d && t.acc.id == 0) {
-                    isqmay[q] = true; // todo p4, confirms same state twice, fix?
-                    if (debug == "1"){std::cout << q << " is Qmay. ";}
+                    isqmay[q] = true; // todo p4, p9 confirms same state twice, fix?
+                    if (debug == "1"){std::cout << "It's Qmay. ";}
                     break;
                 }
             }
@@ -93,24 +94,31 @@ spot::twa_graph_ptr make_semideterministic(VWAA *vwaa, std::string debug) {
             }
             if (!thereIsALoop){
                 isqmust[q] = false;
-                if (debug == "1"){std::cout << q << " is not Qmust. ";}
+                if (debug == "1"){std::cout << "It's not Qmust. ";}
                 break;
             }
         }
 
         // Setting acceptance
 
-        // Since we only work with "yes / no", so we can use the numbers differently:
-        // 0 Means edge is not accepting
-        // 1 Means edge is accepting
-        // 2 Means edge was accepting in vwaa, but will not be accepting in sdba
+        // Since we only work with "yes / no" acceptance, we can use the numbers differently:
+        //  {} = 0 Means edge is not accepting
+        // {0} = 1 Means edge is accepting
+        // {1} = 2 Means edge was accepting in vwaa, but will not be accepting in sdba
         // We remove acceptance marks from all the edges, since no edge in the nondeterministic part is accepting
         for (auto& t: pvwaa->out(q))
         {
-            if (t.acc != 0) {
-                t.acc = 2;
+            for (unsigned d: pvwaa->univ_dests(t.dst)) {
+                if (debug == "1") {
+                    std::cout << "\nEdge " << t.src << "-" << d << " accepting labels: " << t.acc << ". ";
+                }
+                if (t.acc != 0) {
+                    t.acc = 2;
+                    if (debug == "1") { std::cout << "We set acc to " << t.acc << ". "; }
+                }
             }
         }
+        if (debug == "1"){std::cout << "\n";}
 
         // todo how to handle automata that dont accept at all? do we need to bother with them?
     }
@@ -190,7 +198,7 @@ bool checkMayReachableStates(std::shared_ptr<spot::twa_graph> vwaa, std::set<std
                 std::cout << "We are in BADSTATE: " << q << ". "; // todo Better error message
             }
             else {
-                // This state is {} and can not be Qmay nor Qmust // todo Is this right?
+                // This state is {} and hence is Qmust // todo Is this right?
             }
         } else {
             if (isqmay[std::stoi(q)]) {
@@ -241,8 +249,11 @@ void createR(std::shared_ptr<spot::twa_graph> vwaa, unsigned ci, std::set<std::s
             std::cout << "We are in BADSTATE: " << q << ". "; // todo Deal with this
         }
         else {
-            //If the state is {}, we don't have to check if it is Qmay or Qmust. todo if this is true we can make R set of unsigneds and refactor code to a nice one
-            if (debug == "1"){std::cout << "q is {}. ";} // todo keep in mind acceptation
+            //If the state is {}, we know it is Qmust. todo if this is true we can make R set of unsigneds and refactor code to a nice one
+            if (debug == "1"){std::cout << "q is {} (which is Qmust), adding to R. ";} // todo keep in mind acceptation
+            if (R.count(q) == 0) {
+                R.insert(q);
+            }
         }
     } else {
         // If this state is Qmust, we add it (and don't have to check Qmay)
@@ -254,7 +265,7 @@ void createR(std::shared_ptr<spot::twa_graph> vwaa, unsigned ci, std::set<std::s
         } else {
             // If it is Qmay, we recursively call the function and try both adding it and not
             if (isqmay[std::stoi(q)]){
-                if (debug == "1"){std::cout << "It is Qmay (not must!), adding to R ";}
+                if (debug == "1"){std::cout << "It is Qmay (and not Qmust!), adding to R";}
 
                 // We create a new branch with new R and add the state q to this R
                 std::set<std::string> Rx = R;
@@ -264,15 +275,16 @@ void createR(std::shared_ptr<spot::twa_graph> vwaa, unsigned ci, std::set<std::s
 
                 if (!remaining.empty()){
                     // We run the branch that builds the R where this state is added
-                    if (debug == "1"){std::cout << " and creating branch for: " << q << ". ";}
+                    if (debug == "1"){std::cout << " and creating branch including: " << q << ".\n";}
                     createR(vwaa, ci, Conf, remaining, Rx, isqmay, isqmust, sdba, debug);
                 } else{
                     // If this was the last state, we have one R complete. Let's build an R-component from it.
-                    if (debug == "1"){std::cout << " and this was last state, creating Rxcomp for: " << q << ". ";}
+                    if (debug == "1"){std::cout << " and this was lastx state!\n----------> \nCreate Rx comp: \n";}
                     createRComp(vwaa, ci, Conf, Rx, sdba, debug);
+                    if (debug == "1"){std::cout << " \n<---------- \n";}
                 }
                 // We also continue this run without adding this state to R - representing the second branch
-                if (debug == "1"){std::cout<< "Also continuing for q " << q << ". ";}
+                if (debug == "1"){std::cout<< "Also continuing for not adding q to R - ";}
             }
         }
         if (debug == "1"){std::cout << "Done checking for q: " << q;}
@@ -281,9 +293,9 @@ void createR(std::shared_ptr<spot::twa_graph> vwaa, unsigned ci, std::set<std::s
     if (debug == "1"){std::cout << "\n Is this last state? ";}
     // If this was the last state, we have this R complete. Let's build an R-component from it.
     if (remaining.empty()){
-        if (debug == "1"){std::cout << " YES!  \n---------- \nCreate R comp: \n";}
+        if (debug == "1"){std::cout << " YES!  \n----------> \nCreate R comp: \n";}
         createRComp(vwaa, ci, Conf, R, sdba, debug);
-        if (debug == "1"){std::cout << " \n---------- \n";}
+        if (debug == "1"){std::cout << " \n<---------- \n";}
     } else{
         if (debug == "1"){std::cout << " NO! Check another: \n";}
         createR(vwaa, ci, Conf, remaining, R, isqmay, isqmust, sdba, debug);
@@ -306,36 +318,47 @@ void createRComp(std::shared_ptr<spot::twa_graph> vwaa, unsigned ci, std::set<st
         std::cout << " Go:\n";
     }
 
+    // todo what if R is empty?
+
     // Note: "vwaa->num_states()-1" is the last state of the vwaa, which is always the TT state.
 
-    bdd phi1; // todo initialize both to be empty
-    bdd phi2;
+    bdd phi1 = bdd_true();
+    bdd phi2 = bdd_true();
 
     // First we construct the edges from C into the R component by getting the correct phi1 and phi2
     unsigned nq = vwaa->num_states();
 
     for (unsigned q = 0; q < nq; ++q) {
+        if (debug == "1"){std::cout << "\nChecking q: " << q << ". ";}
         if (!(R.find(std::to_string(q)) != R.end())){  // q is not in R, we are doing phi1 part
+            if (debug == "1"){std::cout << "It's not in R. ";}
             // Check if each edge is in the modified transition relation, we only work with those
             for (auto& t: vwaa->out(q)) {
-                if ((Conf.find(std::to_string(q)) != Conf.end()) && t.acc == 2) {
-                    // Replace the edges ending in R with TT
-                    if (R.find(std::to_string(t.dst)) != R.end()){
-                        // todo we might need to reroute this edge not to t.dst but to TT. maybe not.
-                        // t.dst = vwaa->num_states()-1; xz
-                        phi1 = bdd_and(phi1, bdd_ithvar(vwaa->num_states()-1));
-                    } else {
-                        phi1 = bdd_and(phi1, bdd_ithvar(t.dst));  // Keep in mind this destination is q state
-                    }
+                for (unsigned tdst: vwaa->univ_dests(t.dst)) {
+                    if (debug == "1") { std::cout << "E " << t.src << "-" << tdst << " acc:" << t.acc << ". "; }
+                    if ((Conf.find(std::to_string(q)) != Conf.end()) && t.acc == 2) {
+                        if (debug == "1") { std::cout << "q is in Conf and e is acc. "; }
+                        // Replace the edges ending in R with TT
+                        if (R.find(std::to_string(tdst)) != R.end()) {
+                            if (debug == "1") { std::cout << "t.dst is in R. adding TT to phi1"; }
+                            // todo we might need to reroute this edge not to t.dst but to TT. maybe not.
+                            // t.dst = vwaa->num_states()-1; xz
+                            phi1 = bdd_and(phi1, bdd_ithvar(vwaa->num_states() - 1));
+                        } else {
+                            if (debug == "1") { std::cout << "t.dst is not in R. adding t.dst to phi1"; }
+                            phi1 = bdd_and(phi1, bdd_ithvar(tdst));  // Keep in mind this destination is q state
+                        }
 
+                    }
                 }
             }
         } else {  // q is in R, we are doing phi2 part
+            if (debug == "1"){std::cout << "It's in R. adding q to phi2";}
             phi2 = bdd_and(phi2, bdd_ithvar(q));  // Keep in mind this destination is q state
         }
     }
 
-    if (debug == "1"){std::cout << "phi1: " << phi1 << " and 2: " << phi2 << ". ";}
+    if (debug == "1"){std::cout << "\nphi1: " << phi1 << " and 2: " << phi2 << ". ";}
 
     // todo create sdba triplet states from this, check if the added state doesn"t exist already
     // todo create transitions in an R component
